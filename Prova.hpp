b61d2b87548f696b059f28fd426458f71c3f37f8
Arthur Mendonça <135072001+ImArthz@users.noreply.github.com>
2023-11-10 12:42:01 -0300

    Update Prova.hpp

--- Prova.hpp
+++ Prova.hpp
@@ -1,43 +1,43 @@
-#ifndef PROVA_HPP
-#define PROVA_HPP
-#include <iostream>
-#include "Aluno.hpp"
-class PilhaProva; // Forward declaration
-class PilhaAuxiliar; // Forward declaration
-#include <string>
+// #ifndef PROVA_HPP
+// #define PROVA_HPP
+// #include <iostream>
+// #include "Aluno.hpp"
+// class PilhaProva; // Forward declaration
+// class PilhaAuxiliar; // Forward declaration
+// #include <string>
 
-using namespace std;
-class Prova {
-    private:
-    string Disciplina;
-    string Codigo_da_prova;
-    float Nota;
-    Aluno alunos;
-    Prova* prox;
-    PilhaProva* provas;
-    PilhaAuxiliar* auxiliar;
-    public:
-    void setDisciplina(string Disciplina);
-    string getDisciplina();
-    void setCodigo(string Codigo_da_prova);
-    string getCodigo();
-    void setNota(float Nota);
-    float getNota();
-    Prova(string Disciplina,string Codigo_da_prova,float Nota,Aluno alunos,PilhaProva* provas,PilhaAuxiliar* auxiliar);
-    Prova();
-    void setProx(Prova* prox);
-    Prova* getProx();
-    Aluno getAluno();
-    bool adicionarAluno();
-    bool removerAluno(string cpf);
-    bool ImprimirAluno();
-    void cadastrarProvaaux(string Disciplina,string Codigo_da_prova,float Nota,Aluno alunos);
-    void removerProvaaux(string Codigo_da_prova);
-    void voltarpilha();
-    bool imprimirProvas();
-    void removerProvaauxx(string Codigo_da_prova);
-    bool cadastrarProva();
-    bool removerProva(string Codigo_da_prova);
+// using namespace std;
+// class Prova {
+//     private:
+//     string Disciplina;
+//     string Codigo_da_prova;
+//     float Nota;
+//     Aluno alunos;
+//     Prova* prox;
+//     PilhaProva* provas;
+//     PilhaAuxiliar* auxiliar;
+//     public:
+//     void setDisciplina(string Disciplina);
+//     string getDisciplina();
+//     void setCodigo(string Codigo_da_prova);
+//     string getCodigo();
+//     void setNota(float Nota);
+//     float getNota();
+//     Prova(string Disciplina,string Codigo_da_prova,float Nota,Aluno alunos,PilhaProva* provas,PilhaAuxiliar* auxiliar);
+//     Prova();
+//     void setProx(Prova* prox);
+//     Prova* getProx();
+//     Aluno getAluno();
+//     bool adicionarAluno();
+//     bool removerAluno(string cpf);
+//     bool ImprimirAluno();
+//     void cadastrarProvaaux(string Disciplina,string Codigo_da_prova,float Nota,Aluno alunos);
+//     void removerProvaaux(string Codigo_da_prova);
+//     void voltarpilha();
+//     bool imprimirProvas();
+//     void removerProvaauxx(string Codigo_da_prova);
+//     bool cadastrarProva();
+//     bool removerProva(string Codigo_da_prova);
 
-};
-#endif
+// };
+// #endif
