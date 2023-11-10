20c3838a171311bef73f827b373e4cb7c06f6f66
Arthur Mendonça <135072001+ImArthz@users.noreply.github.com>
2023-11-10 12:42:25 -0300

    Update PilhaProva.hpp

--- PilhaProva.hpp
+++ PilhaProva.hpp
@@ -1,26 +1,26 @@
-#ifndef PILHAPROVA_HPP
-#define PILHAPROVA_HPP
-#include <iostream>
-#include "Aluno.hpp"
-#include "Prova.hpp"
-using namespace std;
-class PilhaProva
-{
-private:
-    Prova* top;
-    Prova* bottom;
+// #ifndef PILHAPROVA_HPP
+// #define PILHAPROVA_HPP
+// #include <iostream>
+// #include "Aluno.hpp"
+// #include "Prova.hpp"
+// using namespace std;
+// class PilhaProva
+// {
+// private:
+//     Prova* top;
+//     Prova* bottom;
     
-public:
-    PilhaProva(string Disciplina,string Codigo_da_prova,float Nota,Aluno alunos);
-    PilhaProva();
-    void setTop(Prova* top);
-    Prova* getTop();
-    void setBottom(Prova* bottom);
-    Prova* getBottom();
-    void adicionarProva(string Disciplina,string Codigo_da_prova,float Nota,Aluno alunos);
-    void RemoverProva(string Codigo_da_prova);
-    void ImprimirProva();
-    bool vazia();
+// public:
+//     PilhaProva(string Disciplina,string Codigo_da_prova,float Nota,Aluno alunos);
+//     PilhaProva();
+//     void setTop(Prova* top);
+//     Prova* getTop();
+//     void setBottom(Prova* bottom);
+//     Prova* getBottom();
+//     void adicionarProva(string Disciplina,string Codigo_da_prova,float Nota,Aluno alunos);
+//     void RemoverProva(string Codigo_da_prova);
+//     void ImprimirProva();
+//     bool vazia();
 
-};
-#endif
+// };
+// #endif
